e777fc3d0e0b2a821cdc918dd4ad4910b521facb
Nypline <muhammaddhiyaizza@gmail.com>
2025-05-15 14:56:59 +0700

    rr

--- src/components/KontenList.jsx
+++ src/components/KontenList.jsx
@@ -1,36 +1,35 @@
 import NoteItem from "./NoteItem";
 
-const KontenList = ({ contentIdeas, contentSave, onDelete }) => {
+const KontenList = ({ notes, onDelete }) => {
+  console.log(JSON.stringify(notes));
   return (
     <div className="w-[80%] mx-auto p-6 bg-pink-50 ">
-      {contentIdeas.map((idea) =>
-        idea.isArchived === true ? (
-          <div key={idea.id}>
-            <div className="flex flex-row items-center gap-5 mb-6 mt-10">
-              <img src="/Archive.svg" alt="" className="w-6" />
-              <h1 className="text-3xl font-bold text-gray-800">Archive</h1>
-            </div>
+      <div className="flex flex-row items-center gap-5 mb-6">
+        <img src="/Active.svg" alt="" className="w-6" />
+
+        <h1 className="text-3xl font-bold text-gray-800">Active</h1>
+      </div>
+      {notes.map((note, index) => (
+        <>
+          {note.isArchived === false ? (
+            <>
+              <NoteItem
+                idea={note}
+                img="/Save.svg"
+                colorClass="bg-[#0F7ECD]"
+                onDelete={onDelete}
+              />
+            </>
+          ) : (
             <NoteItem
-              idea={idea}
-              img={"/MoveBack.svg"}
-              colorClass="bg-[#D9D9D9]"
-            />
-          </div>
-        ) : (
-          <div key={idea.id}>
-            <div className="flex flex-row items-center gap-5 mb-6 mt-10">
-              <img src="/Active.svg" alt="" className="w-6" />
-              <h1 className="text-3xl font-bold text-gray-800">Active</h1>
-            </div>
-            <NoteItem
-              idea={idea}
+              idea={note}
               img="/Save.svg"
               colorClass="bg-[#0F7ECD]"
               onDelete={onDelete}
             />
-          </div>
-        )
-      )}
+          )}
+        </>
+      ))}
 
       {/* <div className="flex flex-row items-center gap-5 mb-6">
         <img src="/Active.svg" alt="" className="w-6" />
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -3,68 +3,32 @@ import KontenList from "./KontenList";
 import { useState } from "react";
 
 export default function Notes() {
-  const [contentIdeas, setContentIdeas] = useState([
-    {
-      id: 1,
-      title: "Ide Konten",
-      date: "~Selasa, 13 Mei 2025",
-      tips: "Tips belajar React untuk pemula.",
-      isArchived: false,
-    },
-    {
-      id: 2,
-      title: "Ide Konten",
-      date: "~Selasa, 13 Mei 2025",
-      tips: "Tips belajar React untuk pemula.",
-      isArchived: false,
-    },
-  ]);
-
-  const [contentSave, setContentSave] = useState([
-    {
-      id: 1,
-      title: "Ide Konten",
-
-      date: "~Selasa, 13 Mei 2025",
-      tips: "Tips belajar React untuk pemula.",
-      isArchived: true,
-    },
-    {
-      id: 2,
-      title: "Ide Konten",
-      date: "~Selasa, 13 Mei 2025",
-      tips: "Tips belajar React untuk pemula.",
-      isArchived: true,
-    },
-  ]);
+  const [notes, setNotes] = useState([]);
 
   const onAdd = (title, content) => {
     const newIdea = {
-      id: contentIdeas.length + 1,
+      id: notes.length + 1,
       title: title,
       date: "~Selasa, 13 Mei 2025",
       tips: content,
+      isArchived: false,
     };
     if (!title || !content) {
       alert("Please fill in all fields");
       return;
     }
 
-    setContentIdeas([...contentIdeas, newIdea]);
+    setNotes([...notes, newIdea]);
   };
   const handleDeleteNote = (id) => {
-    const updatedNotes = contentIdeas.filter((note) => note.id !== id);
-    setContentIdeas(updatedNotes);
+    const updatedNotes = notes.filter((note) => note.id !== id);
+    setNotes(updatedNotes);
   };
 
   return (
     <div className="flex flex-col gap-10">
-      <AddNote onAdd={onAdd}></AddNote>
-      <KontenList
-        contentIdeas={contentIdeas}
-        contentSave={contentSave}
-        onDelete={handleDeleteNote}
-      ></KontenList>
+      <AddNote onAdd={onAdd} />
+      <KontenList notes={notes} onDelete={handleDeleteNote} />
     </div>
   );
 }
